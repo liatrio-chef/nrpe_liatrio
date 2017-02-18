dc9b0b18f7e5e30135971f8c8956e7c60096804e
ebracho <eddieb@liatrio.com>
2017-02-18 00:10:23 -0800

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'nrpe_liatrio'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures nrpe_liatrio'
+long_description 'Installs/Configures nrpe_liatrio'
+version '0.1.0'
