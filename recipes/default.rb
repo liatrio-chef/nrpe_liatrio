9b51b64b92f83372c514345cc5a293e97fa8f2d4
ebracho <eddieb@liatrio.com>
2017-02-23 15:03:51 -0800

    Added check_cpu_stats plugin

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,9 +13,18 @@ include_recipe 'nrpe::default'
 include_recipe 'yum-epel'
 package 'nagios-plugins-all'
 package 'nagios-plugins-nrpe'
+package 'sysstat'
 
-remote_file "#{node['nrpe']['plugin_dir']}/check_mem}" do
+remote_file "#{node['nrpe']['plugin_dir']}/check_mem" do
   source 'https://raw.githubusercontent.com/justintime/nagios-plugins/master/check_mem/check_mem.pl'
+  owner 'root'
+  group 'root'
+  mode '755'
+  action :create
+end
+
+remote_file "#{node['nrpe']['plugin_dir']}/check_cpu_stats" do
+  source 'https://raw.githubusercontent.com/etsy/nagios-herald/master/contrib/nrpe-plugins/check_cpu_stats.sh'
   owner 'root'
   group 'root'
   mode '755'
@@ -37,10 +46,16 @@ nrpe_check 'check_disk' do
 end
 
 nrpe_check 'check_mem' do
-  command "#{node['nrpe']['plugin_dir']}/check_mem"
+  command "#{node['nrpe']['plugin_dir']}/check_mem -f"
   warning_condition '5%'
   critical_condition '1%'
   action :add
+end
+
+nrpe_check 'check_cpu_stats' do
+  command "#{node['nrpe']['plugin_dir']}/check_cpu_stats"
+  warning_condition '70,40,30'
+  critical_condition '90,50,40'
 end
 
 service 'nrpe' do
